67135f77da5d860860a2bfc76ae4ac1584ed9657
androka623 <71935603+androka623@users.noreply.github.com>
2023-07-29 12:11:48 -0300

    Add files via upload

---
+++ tipos.js
@@ -0,0 +1,25 @@
+Tipos
+-----
+
+const myNumber = 3;                   // número
+const myString = "Hello, World!"      // string
+const myBoolean = true;               // booleana
+
+Number puede ser tanto coma flotante como entero
+
+const myArray = [];                   // array
+const myObject = {};                  // objeto
+
+Tipos especiales undefined y null:
+
+Si no se la define previamente  -> undefined
+
+const newVariable;
+console.log(newVariable); 	//imprime undefined
+
+Null para variables vacías
+
+const emptyVariable = null;
+console.log(emptyVariable);	// imprime Null
+
+
